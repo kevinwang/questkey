b7dd1f5adf6eeb229805dece644ac90f0625f4d7
Kevin Wang <kevin@kevinwang.com>
2015-02-14 18:29:34 -0500

    Add experience and non-null constraints to User model.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define('User', {
-        username: {type: DataTypes.STRING, unique: true},
-        password: DataTypes.STRING,
-        email: DataTypes.STRING
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        experience: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            defaultValue: 0
+        }
     });
     return User;
 };
